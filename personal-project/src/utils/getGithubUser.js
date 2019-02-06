1d570a114219e3008a0ec283b949a7d7c9fad41c
Jasper Walton <jasper.lyn.walton@gmail.com>
2019-02-06 14:05:40 -0500

    update repos function to filter and format the data; create a function to get, filter, and format the user's pull requests

--- personal-project/src/utils/getGithubUser.js
+++ personal-project/src/utils/getGithubUser.js
@@ -1,1 +1,32 @@
-export const getGithubUser = (username) => fetch(`https://api.github.com/users/${username}`).then(response => response.json());
+export const getGithubUserRepos = username => fetch(`https://api.github.com/users/${username}/repos`)
+  .then(response => response.json())
+  .then(data => {
+    const forked = data.filter(repo => repo.fork === true ? repo : null)
+      .reduce((acc, curr) => 
+        [...acc, 
+          {
+            name: curr.name,
+            url: curr.url,
+          }
+        ]
+      , []);
+  
+    return forked;
+  });
+
+export const getGithubUserPullRequests = username => fetch(`https://api.github.com/users/${username}/events`)
+  .then(response => response.json())
+  .then(data => {
+    const pullRequests = data.filter(event => event.type === 'PullRequestEvent' ? event : null)
+      .reduce((acc, curr) =>
+        [...acc,
+          {
+            title: curr.payload.pull_request.title,
+            url: curr.payload.pull_request.url,
+            status: curr.payload.pull_request.state,
+          }
+        ]
+      , []);
+      
+    return pullRequests;
+  });
